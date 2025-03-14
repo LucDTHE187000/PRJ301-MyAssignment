f347bae5e56744a0d3c5377029f1582c8da3135a
duongtrongluc <lucdthe187000@fpt.edu.vn>
2025-03-15 03:46:31 +0700

    update requestDAO
    thêm hàm insert để lấy dữ liệu

--- MyProject/src/java/dal/RequestDAO.java
+++ MyProject/src/java/dal/RequestDAO.java
@@ -3,27 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import model.Request;
+import java.sql.Date;
 
 /**
  *
  * @author admi
  */
-public class RequestDAO extends DBContext{
+public class RequestDAO extends DBContext {
+
     public List<Request> getAllRequests() {
-    List<Request> requests = new ArrayList<>();
+        List<Request> requests = new ArrayList<>();
         String sql = "SELECT Id, EmployeeId, DateTo, DateFrom, DateCreate, Reason, Status FROM Request";
-        
+
         try {
-             PreparedStatement ps = connection.prepareStatement(sql);
-             
-             ResultSet rs = ps.executeQuery();
-            
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
             while (rs.next()) {
                 Request request = new Request();
                 request.setId(rs.getInt("Id"));
@@ -33,17 +36,31 @@ public class RequestDAO extends DBContext{
                 request.setDateCreate(rs.getDate("DateCreate"));
                 request.setReason(rs.getString("Reason"));
                 request.setStatus(rs.getString("Status"));
-                
+
                 requests.add(request);
             }
-          
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return requests;
     }
+
+    public void insertRequest(Request request) {
+        String sql = "INSERT INTO Request (EmployeeId, DateTo, DateFrom, DateCreate, Reason, Status) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, request.getEmployeeId());
+            ps.setDate(2, request.getDateTo());
+            ps.setDate(3, request.getDateFrom());
+            ps.setDate(4, request.getDateCreate());
+            ps.setString(5, request.getReason());
+            ps.setString(6, request.getStatus());
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-
-
-
